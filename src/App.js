ba85cd8dcddd2dc9ac05bfb5a88b008db4df5fe1
matt-shade-nbcuni <matt.shade@nbcuni.com>
2021-06-18 00:36:34 -0400

    adds name

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ export default function App() {
                 id="people"
                 label="Guests"
                 type="number"
+                name="numGuests"
                 
               />
             </Grid>
