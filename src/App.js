5f9c95b87b71e3df632c4ea8dc67d9f2e7c61969
matt-shade-nbcuni <matt.shade@nbcuni.com>
2021-06-22 00:17:13 -0400

    padding tweaks

--- src/App.js
+++ src/App.js
@@ -42,9 +42,9 @@ export default function App() {
           Would you and your family like to join for a block party located in the cul-de-sac at the end of Pinetree Lane for $50 per family?
         </Typography>
         <Typography component="h3" variant="h6">
-          If yes please fill out the form below so we can determine if there is enough interest. Thank you!
+          If yes please fill out the form below so we can determine if there is enough interest. <strong>Thank you!</strong>
         </Typography>
-        
+        <br/>
         <form name="interested" method="post">
         <input type="hidden" name="form-name" value="interested" />
           <Grid container spacing={2}>
